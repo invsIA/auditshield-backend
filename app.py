4ce6a9d277587c2cdda0e3bc88fa5dfa5b951dfc
AuditShieldAI <shopyourdream6@gmail.com>
2025-06-22 15:40:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return jsonify({"message": "AuditShield AI backend is running"})
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run()
